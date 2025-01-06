ef5c08493b6b36afe511b38c096e1244b2f16755
vallabhdsangvikar18 <vallabhdsangvikar18@gmail.com>
2025-01-06 18:07:25 +0000

    added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  output:'server',
+  adapter: vercelServerless(),
   integrations: [tailwind(), react()]
 });
